99ce5cdf314e8512bfe71f8cdbfa3a8badc1cb12
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-06-29 08:55:22 +0300

    feat: add pie chart component using @nivo/pie
    - Implemented `PieChart` component displaying data using the `ResponsivePie` chart from @nivo/pie.
    - Integrated theme for consistent styling.
    - Added pie chart data for different languages and technologies.
    - Configured chart appearance, legends, and patterns.

---
+++ src/pages/PieChart/PieChartData.js
@@ -0,0 +1,32 @@
+export const PieChartData = [
+  {
+    id: "php",
+    label: "php",
+    value: 200,
+    color: "hsl(169, 70%, 50%)",
+  },
+  {
+    id: "go",
+    label: "go",
+    value: 174,
+    color: "hsl(265, 70%, 50%)",
+  },
+  {
+    id: "stylus",
+    label: "stylus",
+    value: 106,
+    color: "hsl(115, 70%, 50%)",
+  },
+  {
+    id: "ruby",
+    label: "ruby",
+    value: 1,
+    color: "hsl(249, 70%, 50%)",
+  },
+  {
+    id: "rust",
+    label: "rust",
+    value: 298,
+    color: "hsl(352, 70%, 50%)",
+  },
+];
