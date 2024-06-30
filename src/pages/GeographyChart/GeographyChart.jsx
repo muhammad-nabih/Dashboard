f9fd0166e253111edfb0c084b7c8c34ce0d01d8f
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-06-30 10:36:18 +0300

    fix: resolve import PageDescription

--- src/pages/GeographyChart/GeographyChart.jsx
+++ src/pages/GeographyChart/GeographyChart.jsx
@@ -1,13 +1,14 @@
-import PageDescription from "../../components/PageDescription/PageDescription";
+
+// import PageDescription from "../../components/PageDescription/PageDescription";
 import Geography from "./Geography";
 import { Stack } from "@mui/material";
 export default function GeographyChart() {
   return (
     <Stack>
-      <PageDescription
+      {/* <PageDescription
         title={"Geography Chart"}
         description={"Simple Geography Chart"}
-      />
+      /> */}
       <Geography />
     </Stack>
   );
