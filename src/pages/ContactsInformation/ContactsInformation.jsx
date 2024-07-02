aed4aef9895ad721b358d191e3524099789747af
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-07-02 18:59:53 +0300

    Add More Height For ContactsInformation.jsx

--- src/pages/ContactsInformation/ContactsInformation.jsx
+++ src/pages/ContactsInformation/ContactsInformation.jsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 import { columns, rows } from "./ContactInformationData";
 export default function ContactsInformation() {
   return (
-    <Box sx={{ height: "70vh", width: "95%", margin: "0 auto" }}>
+    <Box sx={{ height: "75vh", width: "95%", margin: "0 auto" }}>
       <DataGrid
         slots={{ toolbar: GridToolbar }}
         rows={rows}
